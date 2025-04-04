adff7e6a96b076a7e77a29bb0024f3f9e2910002
Jukalekvi <jukeli5@hotmail.fi>
2025-04-04 11:08:21 +0200

    First changes to test Git connection

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Welcome to our expo app, the SpeakPoint!</Text>
+      <Text style={{textAlign:"center"}}>In here, you can save your daily notes through speaking or texting</Text>
+      <Text>Give it a try with this button:</Text>
+      <Button title="SAVE"></Button>
       <StatusBar style="auto" />
     </View>
   );
