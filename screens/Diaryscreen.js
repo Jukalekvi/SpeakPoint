08591e19e702a5a4e9abb7f559e61d7a11fe0843
Jukalekvi <jukeli5@hotmail.fi>
2025-04-05 07:52:43 +0200

    Updated Diaryscreen to be able to save text in a Flatlist

--- screens/Diaryscreen.js
+++ screens/Diaryscreen.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, Button } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
+import { FlatList, TextInput } from 'react-native-gesture-handler';
 
-const DiaryScreen = () => {
-  const navigation = useNavigation();
+export default function DiaryScreen() {
+
+  const [entry, setEntry] = useState('');
+  const [entries, setEntries] = useState([]);
+
+  const addEntry = () => {
+    if (entry.trim()) {
+      const newEntry = {
+        id: Date.now().toString(),
+        text: entry
+      }
+      setEntries([...entries, newEntry]);
+      setEntry('');
+  }
+};
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Welcome to the Diary!</Text>
-      <Button title="Go Back" onPress={() => navigation.goBack()} />
+      <Text>Welcome to the Diary section! Here you can save the text(for now) for the day which you can also view later</Text>
+      <TextInput
+        placeholder='Write todays thoughts here'
+        value={entry}
+        onChangeText={setEntry}
+        multiline={true}
+      />
+      <Button title="Add a daily entry" onPress={addEntry} />
+      <FlatList
+      data={entries}
+      keyExtractor={(item) => item.id}
+      renderItem={({item}) => (
+        <View>
+          <Text>{item.text}</Text>
+        </View>
+      )}
+      />
     </View>
   );
-};
+}
 
-export default DiaryScreen;
