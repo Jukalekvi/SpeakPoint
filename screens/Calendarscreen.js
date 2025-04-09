47c6f2a65e2d886d07428333c3c1230a88e6d337
Jukalekvi <jukeli5@hotmail.fi>
2025-04-09 14:15:28 +0300

    First prototype of the Calendar added

--- screens/Calendarscreen.js
+++ screens/Calendarscreen.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { View, Text, Button } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { Calendar } from 'react-native-calendars';
 
 const CalendarScreen = () => {
   const navigation = useNavigation();
-
+  //Testdata to see the different colored circles
+  const markedDates = {
+    '2025-04-09': { marked: true, dotColor: 'blue' },
+    '2025-04-08': { marked: true, dotColor: 'green' },
+  };
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Welcome to the Calendar!</Text>
+      <Calendar
+        markedDates={markedDates}
+      />
       <Button title="Go Back" onPress={() => navigation.goBack()} />
     </View>
   );
